696931fbac124027aa73a66f7d5315e26b8608a5
Tommy Tran <tommyktran@gmail.com>
2021-03-11 13:27:05 -0500

    Initial commit

---
+++ 12a/HW12/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ls = new List<string> { "Ant", "Bat", "Cat", "Dog" };
+            int k = 3;
+            GroupAssignments(ls, k);
+        }
+
+        static void GroupAssignments(List<string> ls, int k)
+        {
+            var groups = new List<List<string>>();
+            GroupAssignments(ls, 0, groups, k);
+        }
+
+        static void GroupAssignments(List<string> ls, int idx, List<List<string>> groups, int k)
+        {
+            if (ls.Count == idx)
+            {
+                if (groups.Count == k) PrintGroups(groups);
+                return;
+            }
+            // Add the current element to each existing group
+            for (int i = 0; i < groups.Count; i++)
+            {
+                groups[i].Add(ls[idx]);
+                GroupAssignments(ls, idx + 1, groups, k);
+                groups[i].Remove(ls[idx]);
+            }
+            // Add the current element to a new group
+            groups.Add(new List<string>());
+            groups[groups.Count - 1].Add(ls[idx]);
+            GroupAssignments(ls, idx + 1, groups, k);
+            groups.RemoveAt(groups.Count - 1);
+        }
+
+        static void PrintGroups(List<List<string>> groups)
+        {
+            foreach (List<string> group in groups)
+            {
+                Console.Write("| ");
+                foreach (string s in group)
+                {
+                    Console.Write(s + " ");
+                }
+            }
+            Console.WriteLine("|");
+        }
+    }
+
+}
---
+++ 12c/12c/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _12c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sums(5);
+        }
+        static void Sums(int sum)
+        {
+            Sums(sum, sum, new List<int> { });
+        }
+        static void Sums(int sum, int max, List<int> result)
+        {
+            if (sum == 0)
+            {
+                for (int i = 0; i < result.Count - 1; i++)
+                {
+                    Console.Write(result[i] + " + ");
+                }
+                Console.Write(result[result.Count - 1]);
+                Console.WriteLine();
+                return;
+            }
+            for (int x = Math.Min(sum, max); x > 0; x--)
+            {
+                int tempsum = sum;
+                int tempmax = max;
+                var tempresult = new List<int>(result);
+                sum -= x;
+                max = x;
+                result.Add(x);
+                Sums(sum, max, result);
+                sum = tempsum;
+                max = tempmax;
+                result = tempresult;
+            }
+        }
+    }
+}
